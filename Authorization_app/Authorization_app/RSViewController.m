7846f48836e4c6f55196d5b48b9aea3d87c48c5a
Anna <annaersh1994@gmail.com>
2021-07-03 19:22:03 +0300

    feat: add inactive process

--- Authorization_app/Authorization_app/RSViewController.m
+++ Authorization_app/Authorization_app/RSViewController.m
@@ -73,17 +73,23 @@
     self.authorize.layer.borderWidth = 2;
     self.authorize.layer.cornerRadius = 10;
     UIImage *image = [UIImage new];
-    if (@available(iOS 13.0, *)) {
-        image = [UIImage systemImageNamed:@"person" withConfiguration:[UIImageSymbolConfiguration configurationWithFont:[UIFont systemFontOfSize:13 weight:UIFontWeightSemibold]]];
-    } else {
+    if (@available(iOS 13.0, *))
+        image = [UIImage systemImageNamed:@"person" withConfiguration:[UIImageSymbolConfiguration configurationWithFont:[UIFont systemFontOfSize:13 weight:UIFontWeightBold]]];
+    else
         image = [UIImage imageNamed:@"person.png"];
-    }
     self.authorize.tintColor = color;
     [self.authorize setImage:image forState:UIControlStateNormal];
+    [self.authorize setImage:image forState:UIControlStateDisabled];
     self.authorize.imageEdgeInsets = UIEdgeInsetsMake(10, 0, 10, 0);
     self.authorize.titleEdgeInsets = UIEdgeInsetsMake(10, 5, 10, 0);
     [self.authorize addTarget:self action: @selector(didTapOnButton:) forControlEvents:UIControlEventTouchUpInside];
+    [self.authorize addTarget:self action: @selector(didHighlight) forControlEvents:UIControlEventTouchDown];
 }
+
+- (void)authorizeComfigurationDisable {
+    [self.authorize setAlpha:0.5];
+}
+
 
 - (BOOL)authorizeComfigurationCheck:(UITextField *)text withData:(NSString *)dataText{
     BOOL check = NO;
@@ -91,12 +97,12 @@
     UIColor *colorRight = [UIColor colorWithRed:0.3 green:0.36 blue:0.41 alpha:1.0];
     if (![text.text isEqualToString: dataText])
     {
-        NSLog(@"FAIL");
+        //NSLog(@"FAIL");
         text.layer.borderColor = colorWrong.CGColor;
     }
     else
     {
-        NSLog(@"SUCCESS");
+        //NSLog(@"SUCCESS");
         text.layer.borderColor = colorRight.CGColor;
         check = YES;
     }
@@ -107,7 +113,7 @@
     sender.text = @"";
     UIColor *colorRight = [UIColor colorWithRed:0.3 green:0.36 blue:0.41 alpha:1.0];
     sender.layer.borderColor = colorRight.CGColor;
-    NSLog(@"tapping");
+    //NSLog(@"tapping");
 }
 
 - (void)keyboardWillHide:(id)sender {
@@ -130,24 +136,53 @@
     CGPoint location = [sender locationInView:self.view];
     BOOL isPointInsideButton = CGRectContainsPoint(self.authorize.frame, location);
     if (isPointInsideButton)
-    {
         [self.authorize endEditing: NO];
-    }
     else
         [self.view endEditing: YES];
 }
 
 - (void)didTapOnButton:(UITapGestureRecognizer *)sender {
-    NSLog(@"BUTTON");
     Data *data = [Data new];
+    [self.authorize setBackgroundColor:[UIColor clearColor]];
+    [self.authorize.titleLabel setAlpha:1.0];
+    [self.authorize.imageView setAlpha:1.0];
     BOOL loginCheck = [self authorizeComfigurationCheck: self.login withData: data.login];
     BOOL passwordCheck = [self authorizeComfigurationCheck: self.password withData: data.password];
     if (loginCheck && passwordCheck)
     {
+        [self.view endEditing:YES];
+        [self authorizeComfigurationDisable];
+        self.authorize.userInteractionEnabled = NO;
+        self.login.userInteractionEnabled = NO;
+        self.password.userInteractionEnabled = NO;
+        self.authorize.tintAdjustmentMode = UIViewTintAdjustmentModeNormal;
         
     }
     else
-        [self.authorize endEditing: NO];
+    {
+        [self.authorize endEditing:NO];
+    }
+}
+
+- (void)didHighlight {
+    if (self.authorize.isHighlighted && self.authorize.isEnabled)
+    {
+        [self.authorize setBackgroundColor:[UIColor colorWithRed:0.50 green:0.64 blue:0.93 alpha:0.2]];
+        [self.authorize.imageView setAlpha:1.0];
+        [self.authorize.titleLabel setAlpha:0.4];
+        UIImage *imageFill = [UIImage new];
+        if (@available(iOS 13.0, *))
+            imageFill = [UIImage systemImageNamed:@"person.fill" withConfiguration:[UIImageSymbolConfiguration configurationWithFont:[UIFont systemFontOfSize:13 weight:UIFontWeightBold]]];
+        else
+            imageFill = [UIImage imageNamed:@"person_fill.png"];
+        [self.authorize setImage:imageFill forState:UIControlStateHighlighted];
+    }
+    else
+    {
+        [self.authorize.titleLabel setAlpha:1.0];
+        [self.authorize.imageView setAlpha:1.0];
+        [self.authorize setBackgroundColor:[UIColor clearColor]];
+    }
 }
 
 @end
